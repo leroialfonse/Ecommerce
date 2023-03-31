1e73ccedbc3a500dcc53fad2c92540df2269dfb8
Brandon White <brandonkwhite3@gmail.com>
2023-03-31 00:01:49 -0400

    added the item contoller functionality.

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,4 @@
 const Item = require('../models/Item');
-const Itme = require('../models/Item');
 
 // Get all the items from the DB
 
@@ -9,4 +8,24 @@ module.exports.getItems = (req, res) => {
 }
 
 // Add new items into the cart. 
+module.exports.postItem = (req, res) => {
+    const newItem = new Item(req.body);
+    newItem.save().then(item => res.json(item));
+}
+
+// Update items
+module.exports.updateItem = (req, res) => {
+    Item.findByIdAndUpdate({ _id: req.params.id }, req.body).then((item) => {
+        Item.findOne({ _id: req.parms.id }).then(function (item) {
+            res.json(item);
+        });
+    });
+}
+
+// Delete Items
+module.exports.deleteItem = (req, res) => {
+    Item.findByIdAndDelete({ _id: req.params.id }).then((item) => {
+        res.json({ success: true });
+    });
+}
 
